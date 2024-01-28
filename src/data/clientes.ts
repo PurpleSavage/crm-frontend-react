595e995362dd635aa1704f1a4f15555b125baaf4
PurpleSavage <jzuritapalomino@gmail.com>
2024-01-28 10:22:51 -0500

    change enviroments

--- src/data/clientes.ts
+++ src/data/clientes.ts
@@ -2,13 +2,13 @@ import { objClientes } from "../types/typesClientes"
 import { ClienteDataInterface } from "../types/typesClientes"
 
 export async function obtenerCLientes():Promise<objClientes[]>{
-    const url = `${import.meta.env.VITE_FRONTEND_URL}/all-clients`
+    const url = `${import.meta.env.VITE_BACKEND_URL}/all-clients`
     const respuesta = await fetch(url)
     const resultado =await respuesta.json()
     return resultado
 }
 export async function obtenerCLiente(id:number):Promise<objClientes[]>{
-    const url = `${import.meta.env.VITE_FRONTEND_URL}/client/${id}`
+    const url = `${import.meta.env.VITE_BACKEND_URL}/client/${id}`
     const respuesta = await fetch(url)
     const resultado =await respuesta.json()
     return resultado
@@ -16,7 +16,7 @@ export async function obtenerCLiente(id:number):Promise<objClientes[]>{
 
 
 export async function agregarCliente(datos:ClienteDataInterface) {
-    const url = `${import.meta.env.VITE_FRONTEND_URL}/add-client`
+    const url = `${import.meta.env.VITE_BACKEND_URL}/add-client`
     try {
         const respuesta =await fetch(url,{
             method:'POST',
@@ -31,7 +31,7 @@ export async function agregarCliente(datos:ClienteDataInterface) {
     }
 }
 export async function actualizarCliente(id:number,datos:ClienteDataInterface){
-    const url = `${import.meta.env.VITE_FRONTEND_URL}/update-client/${id}`
+    const url = `${import.meta.env.VITE_BACKEND_URL}/update-client/${id}`
     try {
         const respuesta =await fetch(url,{
             method:'PUT',
@@ -47,7 +47,7 @@ export async function actualizarCliente(id:number,datos:ClienteDataInterface){
 }
 export async function eliminarCliente(id:number){
 
- const url = `${import.meta.env.VITE_FRONTEND_URL}/delete-client/${id}`
+ const url = `${import.meta.env.VITE_BACKEND_URL}/delete-client/${id}`
     try {
         const respuesta =await fetch(url,{
             method:'DELETE',
